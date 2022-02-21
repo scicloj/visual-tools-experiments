b010e4a2f254aaebe5bd60246fd121941f6ae89d
daslu <daniel.slutsky@gmail.com>
2022-02-21 23:18:34 +0200

    examples, screenshot

--- portal-nrepl/src/example.clj
+++ portal-nrepl/src/example.clj
@@ -10,3 +10,35 @@
 [:portal.viewer/code "{:x 9 :y [(+ 1 2)]}"]
 
 [:portal.viewer/tree {:x 9 :y [(+ 1 2)]}]
+
+
+
+(defn random-tree [max-depth]
+  (if (pos? max-depth)
+    [(random-tree (- max-depth (rand-int 2)))
+     (random-tree (- max-depth (rand-int 2)))]
+    []))
+
+[:portal.viewer/tree (random-tree 2)]
+
+
+
+[:h1
+ "A plot:"
+ [:portal.viewer/vega-lite
+  {:data {:values
+          (->> (repeatedly 200 #(- (rand) 0.5))
+               (reductions +)
+               (map-indexed (fn [x y]
+                              {:w (rand-int 9)
+                               :z (rand-int 9)
+                               :x x
+                               :y y})))},
+   :width 200,
+   :height 200,
+   :mark "point"
+   :encoding
+   {:size {:field "w" :type "quantitative"}
+    :x {:field "x", :type "quantitative"},
+    :y {:field "y", :type "quantitative"},
+    :fill {:field "z", :type "nominal"}}}]]
